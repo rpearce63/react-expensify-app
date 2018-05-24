7b57f886dbd37c53e579bba3e36d79e9b980a83c
Rick Pearce <rick.pearce.crzs@statefarm.com>
2018-05-24 17:21:59 -0500

    add react-redux to connect components to store

--- src/app.js
+++ src/app.js
@@ -1,7 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<AppRouter /> , document.getElementById('app'));
+const store = configureStore();
+let state;
+store.subscribe(() => {
+    state = store.getState();
+    //console.log(state);
+})
+
+store.dispatch(addExpense({ description: 'Water bill', amount: 10000}));
+store.dispatch(addExpense({description: 'Gas bill', amount: 4000}));
+
+store.dispatch(setTextFilter('water'));
+
+setTimeout(() => {
+    store.dispatch(setTextFilter('rent'));
+}, 3000);
+
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
+
+ReactDOM.render(jsx , document.getElementById('app'));
--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const EditExpensePage = (props) => {
-    console.log(props);
    return (
         <div>
             Edit expense with id {props.match.params.id}

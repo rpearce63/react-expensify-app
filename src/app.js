9625dea07ba14ae1b3a01cf596935876e77d7f40
Rick Pearce <rpearce63@yahoo.com>
2018-06-08 07:06:41 -0500

    add redirect on login/out

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import AppRouter from './routers/AppRouter';
+import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
@@ -11,7 +11,7 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import {firebase} from './firebase/firebase';
-//import './playground/promises';
+
 
 const store = configureStore();
 
@@ -20,18 +20,30 @@ const jsx = (
         <AppRouter />
     </Provider>
 );
+let hasRendered = false;
+const renderApp = () => {
+    if (!hasRendered) {
+        ReactDOM.render(jsx , document.getElementById('app'));
+        hasRendered = true;
+    }
+};
 
 ReactDOM.render(<p>Loading...</p> , document.getElementById('app'));
 
-store.dispatch(startSetExpenses()).then(() => {
-    ReactDOM.render(jsx , document.getElementById('app'));
-});
+
 
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
-        console.log('log in.');
+        console.log('logged in.');
+        store.dispatch(startSetExpenses()).then(() => {
+            renderApp();
+            if (history.location.path === '/') {
+                history.push('/dashboard');
+            }
+        });
     } else {
-        console.log('log out');
+        renderApp();
+        history.push('/');
     }
 });
 
